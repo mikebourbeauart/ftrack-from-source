966ef38000735a20554965ecfa9dc74f708e2542
Mike Bourbeau <borbs727@gmail.com>
2016-08-04 15:11:44 -0400

    setting up launcher

---
+++ mb_tests/mb_tests/bin/launcher.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import subprocess
+import logging
+
+_ROOT_DIR = os.path.split(os.path.dirname(os.path.realpath(__file__)))[0]
+
+_LOGS_DIR = os.path.join(_ROOT_DIR, 'logs') 
+
+if not os.path.exists(_LOGS_DIR):
+	os.makedirs(_LOGS_DIR) 
+
+logger = logging.getLogger(__name__)
+handler = logging.FileHandler('%s/launcher_log.log' % _LOGS_DIR, mode='w')
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+handler.setFormatter(formatter)
+logger.addHandler(handler) 
+logger.setLevel(logging.INFO)
+
+
+logger.info('Launching Ftrack...')
+
+subprocess.Popen(['python', '-m', 'ftrack_connect'], env=envs)
